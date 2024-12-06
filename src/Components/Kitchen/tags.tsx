821a7c6df7a2620d95540dd20136c82b25cd6661
AKOSSETAN Léa <tayeakossetan@gmail.com>
2024-12-06 19:06:25 +0100

    Modification1

---
+++ src/Components/Kitchen/tags.tsx
@@ -0,0 +1,29 @@
+// import React from 'react';
+// import './Tags.css'; 
+
+// function Tags {
+//     const tags = [
+//         "appliances", "architecture", "area", "bed", "bedroom", "chair", "chairs",
+//         "corners", "decor", "design", "exhaust", "fire place", "frames", "furniture",
+//         "garden", "home", "interior", "kitchen", "lifestyle", "lights", "living room",
+//         "modern", "outdoor", "plants", "shopping", "sitout", "table", "table top", "wall"
+//       ];
+    
+//     const Tags = ({ tags }) => {
+//       return (
+//         <div className="tags-container">
+//           <h2>Tags</h2>
+//           <div className="tags-grid">
+//             {tags.map((tag, index) => (
+//               <span key={index} className="tag">
+//                 {tag}
+//               </span>
+//             ))}
+//           </div>
+//         </div>
+//       );
+//     };
+// }
+
+
+// export default Tags;
